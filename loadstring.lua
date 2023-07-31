5ddf8e8f4c393b037f2ba610b9ac573e7b035308
ScriptMaster <125311491+TheScriptMaster1@users.noreply.github.com>
2023-07-31 17:55:23 +0200

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,8 @@
+local supported_games = {
+	11251823801, -- Slap Battles but Bad
+   11251988911 -- Slap Royale but Bad
+}
+
+if table.find(supported_games, game.PlaceId) then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/TheScriptMaster1/zyer-hub/main/"..game.PlaceId..".lua"))()
+end
